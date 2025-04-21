cc9ba6dd927c235edc12117fd5445848874fb45f
alonit <alon.itach@mail.huji.ac.il>
2025-04-21 23:34:32 +0300

    initial commit

---
+++ katas/hello_world.py
@@ -0,0 +1,9 @@
+def hello_world():
+    """
+    Returns the string "hello world".
+    """
+
+
+if __name__ == '__main__':
+    result_str = hello_world()
+    print(result_str)   # hello world should be printed
